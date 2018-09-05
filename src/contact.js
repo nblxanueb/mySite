9aa3c442cddc95ce8a9151548de52c8110d0521b
andre <a.e.pykhantsev@gmail.com>
2018-09-05 05:19:43 -0400

    added @media styles

--- src/contact.js
+++ src/contact.js
@@ -2,6 +2,7 @@ import React from "react";
 import linkedin from "./images/linkedin.png";
 import github from "./images/git-logo-w.png";
 import flickr from "./images/flickr.png";
+import email from "./images/email.png";
 import { SocialIcon } from "react-social-icons";
 
 function Contact() {
@@ -10,17 +11,27 @@ function Contact() {
         <div>
           <h3> contact information: </h3>
         </div>
-        <div>
-          <a href="https://www.linkedin.com/in/andrey-pykhantsev/">
-            <img src={linkedin} alt="linkedin" className="contact-icons" />
-          </a>
-          <a href="https://github.com/nblxanueb">
-            <img src={github} alt="github" className="contact-icons" />
-          </a>
-          <a href="https://www.flickr.com/photos/pykhantsev/">
-            <img src={flickr} alt="flickr" className="contact-icons" />
-          </a>
-          <SocialIcon className="icon" url={`mailto:a.e.pykhantsev@gmail.com`} />
+        <div className="social-media">
+          <div>
+            <a href="https://www.linkedin.com/in/andrey-pykhantsev/">
+              <img src={linkedin} alt="linkedin" className="contact-icons" />
+            </a>
+          </div>
+          <div>
+            <a href="https://github.com/nblxanueb">
+              <img src={github} alt="github" className="contact-icons" />
+            </a>
+          </div>
+          <div>
+            <a href="https://www.flickr.com/photos/pykhantsev/">
+              <img src={flickr} alt="flickr" className="contact-icons" />
+            </a>
+          </div>
+          <div>
+            <a href="mailto:a.pykhantsev@gmail.com">
+              <img src={email} alt="email" className="contact-icons" />
+            </a>
+          </div>
         </div>
         <div>
           <h6> made by A.Pykhantsev © 2018 </h6>
